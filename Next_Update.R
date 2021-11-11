c109db99588135b0822dff9f640ab58b5be1858d
caterinap <caterina.penone@gmail.com>
2021-11-11 17:45:25 +0100

    Added todo file

---
+++ Next_Update.R
@@ -0,0 +1,6 @@
+# This script collects all things to do during the next update of the dataset
+# Script by: Caterina Penone
+
+
+#TODO in GRASSLAND DATASET:
+# add oomycota and change protist_oomycota, cercozoa (also in OTU name)
