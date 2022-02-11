30cfdf7ed92b9ffa5531d9cd5d48070da1be6dd1
Caterina Penone <caterinap@users.noreply.github.com>
2022-02-11 18:05:02 +0100

    Update Next_Update.R

--- Next_Update.R
+++ Next_Update.R
@@ -7,5 +7,10 @@
 # change data type for bats to "pass_number"
 # homogenise trophic level names with forest dataset (e.g. these, but check others too)
 tr[Group_broad == "bryophyte", Group_broad:= "Bryophyte"]
-tr[Group_broad == "bat", Group_broad:= "Bat"]
+tr[Group_broad == "Bats", Group_broad:= "Bat"]
+tr[Group_broad == "Birds", Group_broad:= "Bird"]
+tr[Group_broad == "soilfungi", Group_broad:= "Soilfungi"]
 #for Group_broad use Arthropod or use Araneae, Coleoptera... etc?
+tr[Group_fine == "Bats", Group_fine:= "Bat"]
+tr[Group_fine == "Birds", Group_fine:= "Bird"]
+#check also all Fun_group_broad and fine (cf forest script 210217_ForestDivDataUPDATE.R)
